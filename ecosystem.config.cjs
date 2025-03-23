e566e4cc305180e13c9385d1cdc09f5a3483fa9d
Jordan <olivier.jordan21@gmail.com>
2025-03-23 19:27:59 +0100

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,23 +1,24 @@
 module.exports = {
   apps: [
     {
-      name: 'backend',
-      cwd: './server',
-      script: 'dist/main.js',
+      name: "backend",
+      cwd: "./server",
+      script: "dist/main.js",
       env: {
-        NODE_ENV: 'production',
-        PORT: 4021
-      }
+        NODE_ENV: "production",
+        PORT: 4021,
+      },
     },
     {
-      name: 'frontend',
-      cwd: './client',
-      script: '.output/server/index.mjs',
+      name: "frontend",
+      cwd: "./client",
+      script: ".output/server/index.mjs",
+      port: 3021,
       env: {
-        NODE_ENV: 'production',
+        NODE_ENV: "production",
         PORT: 3021,
-        HOST: '0.0.0.0'
-      }
-    }
-  ]
-}
+        HOST: "0.0.0.0",
+      },
+    },
+  ],
+};
